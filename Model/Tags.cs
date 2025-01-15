7b9a5d95fede573ea7bd96d64ca6e1fd07f6ce2b
arria11 <arya96885@gmail.com>
2025-01-15 13:33:41 +0545

    Filter by tags functionality added with new tag model class.

---
+++ Model/Tags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Model
+{
+    public class Tags
+    {
+        public string? TagOption { get; set; } = string.Empty;
+        public string? CustomTagOption { get; set; } = string.Empty;
+    }
+}
