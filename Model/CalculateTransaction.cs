68b2888a9509d3180379d058f65fcdbfafcf112a
arria11 <arya96885@gmail.com>
2025-01-15 18:18:20 +0545

    separated calculate transaction functionality to reduce redundancy

--- Model/CalculateTransaction.cs
+++ Model/CalculateTransaction.cs
@@ -5,10 +5,13 @@ namespace ExpenseTracker.Model
 {
     public class CalculateTransaction
     {
-        public decimal TotalInflow { get; set; }
+        public decimal InflowAmount { get; set; }
         public decimal TotalOutflow { get; set; }
         public decimal TotalDebt { get; set; }
         public decimal PendingDebt { get; set; }
 
+        public decimal ClearedDebt { get; set; }
+
+        public decimal ClearedInflow { get; set; }
     }
 }
---
+++ Services/CalculateTransactionService.cs
@@ -0,0 +1,35 @@
+﻿using ExpenseTracker.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpenseTracker.Services
+{
+    public class CalculateTransactionService
+    {
+
+        public CalculateTransaction CalculateTransactionTotals(List<TransactionItem> transactions)
+        {
+            if (transactions == null || !transactions.Any())
+            {
+                // Handle empty or null transaction list
+                return new CalculateTransaction(); // Return default values
+            }
+            var inflowAmount = transactions.Where(t => t.Type == "Inflow").Sum(t => t.Amount);
+            var totalOutflow = transactions.Where(t => t.Type == "Outflow").Sum(t => t.Amount);
+            var totalDebt = transactions.Where(t => t.Type == "Debt").Sum(t => t.Amount);
+            var clearedDebt = transactions.Where(t => t.Type == "Debt" && t.IsCleared).Sum(t => t.Amount);
+            var pendingDebt = totalDebt - clearedDebt;
+            var clearedInflow = inflowAmount - clearedDebt;
+
+            return new CalculateTransaction
+            {
+                InflowAmount = inflowAmount,
+                TotalOutflow = totalOutflow,
+                TotalDebt = totalDebt,
+                PendingDebt = pendingDebt,
+                ClearedDebt = clearedDebt,
+                ClearedInflow = clearedInflow
+            };
+        }
+    }
+}
