b8040cbdca4de55c9b6e12a4c747bbc20fd447e2
arria11 <arya96885@gmail.com>
2025-01-17 00:34:37 +0545

    created default tags

---
+++ Services/TagService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public class TagService
+    {
+        public List<string> GetTags()
+        {
+            return new List<string>
+            {
+                "Yearly",
+                "Monthly",
+                "Food",
+                "Drinks",
+                "Clothes",
+                "Gadgets",
+                "Miscellaneous",
+                "Fuel",
+                "Rent",
+                "EMI",
+                "Party",
+                "Salary",
+                "Shopping",
+                "Other"
+            };
+        }
+    }
+}
